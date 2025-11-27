eb089462eb445c35cdf54b900e04c3ab3437608c
Paul <betinellipaul@gmail.com>
2025-11-27 08:47:38 +0100

    Organisation du repo

--- algoSAE/src/classe/ForceBrute.java
+++
@@ -1,8 +0,0 @@
-package classe;
-
-import java.util.*;
-
-public class ForceBrute {
-	private double bestScore = Double.POSITIVE_INFINITY;
-    private List<Groupe> bestSolution = new ArrayList<>();
-}
